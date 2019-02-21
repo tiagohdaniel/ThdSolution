f3b09e601e501270de66f9a59e0b8591cb15a204
Tiago Daniel <tiagohdaniel@gmail.com>
2019-02-20 22:15:05 -0300

    OrderApi

--- OrderApi/Model/Customer.php
+++ OrderApi/Model/Customer.php
@@ -1,21 +1,15 @@
 <?php
 /**
- * Saraiva Platform | Fcamara
- *
  * NOTICE OF LICENSE
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.fcamara.com.br for more information.
+ * versions in the future.
  *
- * @category  ${MAGENTO_MODULE_NAMESPACE}
- * @package   ${MAGENTO_MODULE_NAMESPACE}_${MAGENTO_MODULE}
+ * @copyright Copyright (c) 2019 ThdSolution
  *
- * @copyright Copyright (c) 2019 Fcamara - Saraiva Platform (http://www.fcamara.com.br)
- *
- * @author    Tiago Daniel <tiago.daniel@fcamara.com.br>
+ * @author    Tiago Daniel <tiagohdaniel@gmail.com>
  */
 
 namespace ThdSolution\OrderApi\Model;
