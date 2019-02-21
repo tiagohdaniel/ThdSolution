2dd97bba702713d4daa6c1c32223ad6804da9c95
Tiago Daniel <tiagohdaniel@gmail.com>
2019-02-20 22:13:22 -0300

    OrderApi

---
+++ OrderApi/Model/Customer.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Saraiva Platform | Fcamara
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.fcamara.com.br for more information.
+ *
+ * @category  ${MAGENTO_MODULE_NAMESPACE}
+ * @package   ${MAGENTO_MODULE_NAMESPACE}_${MAGENTO_MODULE}
+ *
+ * @copyright Copyright (c) 2019 Fcamara - Saraiva Platform (http://www.fcamara.com.br)
+ *
+ * @author    Tiago Daniel <tiago.daniel@fcamara.com.br>
+ */
+
+namespace ThdSolution\OrderApi\Model;
+
+
+class Customer
+{
+    /**
+     * @var \Magento\Customer\Model\AccountManagementFactory
+     */
+    private $_accountManagementFactory;
+
+    /**
+     * @var \Magento\Customer\Model\AddressFactory
+     */
+    private $_addressFactory;
+
+    public function __construct
+    (
+        \Magento\Customer\Model\AccountManagementFactory    $accountManagementFactory,
+        \Magento\Customer\Api\Data\CustomerInterfaceFactory $customer,
+        CustomerTokenFactory                                $customerToken,
+        \Magento\Customer\Model\AddressFactory              $addressFactory
+    )
+    {
+        $this->_accountManagementFactory  = $accountManagementFactory;
+        $this->_customerInterfaceFactory  = $customer;
+        $this->_customerTokenFactory      = $customerToken;
+        $this->_addressFactory            = $addressFactory;
+    }
+
+    /**
+     * Setting customer data
+     *
+     * @param $customerData
+     * @param $password
+     * @return \Magento\Customer\Api\Data\CustomerInterface
+     */
+    public function setCustomerData($customerData, $password)
+    {
+        /** @var \Magento\Customer\Model\AccountManagement $accountManagement */
+        $accountManagement  = $this->_accountManagementFactory->create();
+
+        /** @var \Magento\Customer\Api\Data\CustomerInterfaceFactory $customer */
+        $customer           = $this->_customerInterfaceFactory->create();
+
+        $customer->setEmail($customerData['email']);
+        $customer->setFirstname($customerData['firstname']);
+        $customer->setLastname($customerData['lastname']);
+        $addressData        = $this->_setCustomerAddressData($customerData['addresses']);
+        $customer->setData('addresses', $addressData);
+        $output             = $accountManagement->createAccount($customer, $password);
+
+        /** @var  $customerToken */
+        $customerToken      = $this->_customerTokenFactory->create();
+        $token              = $customerToken->createCustomerAccessToken($customer->getEmail(), $password);
+
+        return $output;
+    }
+
+    /**
+     * Setting Address data for the customer
+     *
+     * @param $addressData
+     * @return \Magento\Customer\Model\AddressFactory
+     */
+    private function _setCustomerAddressData($addressData)
+    {
+        /** @var \Magento\Customer\Model\AddressFactory $address */
+        $address            = $this->_addressFactory->create();
+
+        foreach($addressData as $data){
+            $address->setDefaultShipping($data['defaultShipping']);
+            $address->setDefaultBilling($data['defaultBilling']);
+            $address->setFirstName($data['firstname']);
+            $address->setLastName($data['lastname']);
+            $address->setRegionCode($data['region']['regionCode']);
+            $address->setRegione($data['region']['region']);
+            $address->setRegionId($data['region']['regionId']);
+            $address->setPostcode($data['postcode']);
+            $address->setStreet($data['street'][0]);
+            $address->setCity($data['city']);
+            $address->setTelephone($data['telephone']);
+            $address->setCountryId($data['countryId']);
+        }
+
+        return $address;
+    }
+
+}
--- OrderApi/Model/OrderPlace.php
+++ OrderApi/Model/OrderPlace.php
@@ -28,19 +28,9 @@ class OrderPlace
     private $_bodyParams;
 
     /**
-     * @var \Magento\Customer\Model\CustomerFactory
-     */
-    private $_customerFactory;
-
-    /**
      * @var Rest\RequestFactory
      */
     private $_requestFactory;
-
-    /**
-     * @var \Magento\Customer\Model\AccountManagementFactory
-     */
-    private $_accountManagementFactory;
 
     /**
      * @var ServiceInputProcessor
@@ -51,11 +41,6 @@ class OrderPlace
      * @var Rest\InputParamsResolverFactory
      */
     private $_rest;
-
-    /**
-     * @var \Magento\Customer\Model\AddressFactory
-     */
-    private $_addressFactory;
 
     /**
      * @var FieldsFilter
@@ -73,113 +58,51 @@ class OrderPlace
     private $_serviceOutputProcessor;
 
     /**
-     * @var CustomerTokenFactory
-     */
-    private $_customerTokenFactory;
-
-    /**
      * @var QuoteManagement
      */
     private $_quoteManagement;
 
+    /**
+     * @var CustomerFactory
+     */
+    private $_customer;
+
     public function __construct
     (
-        \Magento\Customer\Model\CustomerFactory                    $customerFactory,
-        \Magento\Framework\Webapi\Rest\RequestFactory              $requestFactory,
-        \Magento\Customer\Model\AccountManagementFactory           $accountManagementFactory,
-        \Magento\Customer\Api\Data\CustomerInterfaceFactory        $customer,
-        ServiceInputProcessor                                      $serviceInputProcessor,
-        \Magento\Webapi\Controller\Rest\InputParamsResolver        $rest,
-        \Magento\Customer\Model\AddressFactory                     $addressFactory,
-        FieldsFilter                                               $fieldsFilter,
-        RestResponse                                               $response,
-        ServiceOutputProcessor                                     $serviceOutputProcessor,
-        \ThdSolution\OrderApi\Model\CustomerToken                  $customerToken,
-        QuoteManagementFactory                                     $quoteManagement
-
+        \Magento\Framework\Webapi\Rest\RequestFactory       $requestFactory,
+        ServiceInputProcessor                               $serviceInputProcessor,
+        \Magento\Webapi\Controller\Rest\InputParamsResolver $rest,
+        FieldsFilter                                        $fieldsFilter,
+        RestResponse                                        $response,
+        ServiceOutputProcessor                              $serviceOutputProcessor,
+        QuoteManagementFactory                              $quoteManagement,
+        CustomerFactory                                     $customer
     )
     {
-        $this->_customerFactory           = $customerFactory;
         $this->_requestFactory            = $requestFactory;
-        $this->_accountManagementFactory  = $accountManagementFactory;
-        $this->_customerInterfaceFactory  = $customer;
         $this->_serviceInputProcessor     = $serviceInputProcessor;
         $this->_rest                      = $rest;
-        $this->_addressFactory            = $addressFactory;
         $this->_fieldsFilter              = $fieldsFilter;
         $this->_response                  = $response;
         $this->_serviceOutputProcessor    = $serviceOutputProcessor;
-        $this->_customerTokenFactory      = $customerToken;
         $this->_quoteManagement           = $quoteManagement;
+        $this->_customer                  = $customer;
     }
 
     public function placeOrder()
     {
         $requestObj             =  $this->_requestFactory->create();
         $this->_bodyParams      = $requestObj->getBodyParams();
-        $customer               = $this->setCustomerData($this->_bodyParams['customer'], $this->_bodyParams['password']);
-        $customerToken          = $this->_customerTokenFactory->createCustomerAccessToken
-                                    ($customer->getEmail(), $this->_bodyParams["password"]);
+
+        /** @var  $customerFactory */
+        $customerFactory        = $this->_customer->create();
+        $customer               = $customerFactory->setCustomerData($this->_bodyParams['customer'], $this->_bodyParams['password']);
 
         /** @var  $quoteManagementFactory */
         $quoteManagementFactory = $this->_quoteManagement->create();
 
         $quoteId                = $quoteManagementFactory->createEmptyCartForCustomer($customer->getId());
         $quoteItemObj           = $quoteManagementFactory->addProducts($this->_bodyParams['cartItem'], $quoteId);
-    }
-
-    /**
-     * Setting customer data
-     *
-     * @param $customerData
-     * @param $password
-     * @return \Magento\Customer\Api\Data\CustomerInterface
-     */
-    public function setCustomerData($customerData, $password)
-    {
-        /** @var \Magento\Customer\Model\AccountManagement $accountManagement */
-        $accountManagement  = $this->_accountManagementFactory->create();
-
-        /** @var \Magento\Customer\Api\Data\CustomerInterfaceFactory $customer */
-        $customer           = $this->_customerInterfaceFactory->create();
-
-        $customer->setEmail($customerData['email']);
-        $customer->setFirstname($customerData['firstname']);
-        $customer->setLastname($customerData['lastname']);
-        $addressData        = $this->_setCustomerAddressData($customerData['addresses']);
-        $customer->setData('addresses', $addressData);
-        $output             = $accountManagement->createAccount($customer, $password);
-
-        return $output;
-    }
-
-    /**
-     * Setting Address data for the customer
-     *
-     * @param $addressData
-     * @return \Magento\Customer\Model\AddressFactory
-     */
-    private function _setCustomerAddressData($addressData)
-    {
-        /** @var \Magento\Customer\Model\AddressFactory $address */
-        $address            = $this->_addressFactory->create();
-
-        foreach($addressData as $data){
-            $address->setDefaultShipping($data['defaultShipping']);
-            $address->setDefaultBilling($data['defaultBilling']);
-            $address->setFirstName($data['firstname']);
-            $address->setLastName($data['lastname']);
-            $address->setRegionCode($data['region']['regionCode']);
-            $address->setRegione($data['region']['region']);
-            $address->setRegionId($data['region']['regionId']);
-            $address->setPostcode($data['postcode']);
-            $address->setStreet($data['street'][0]);
-            $address->setCity($data['city']);
-            $address->setTelephone($data['telephone']);
-            $address->setCountryId($data['countryId']);
-        }
-
-        return $address;
     }
 
 }
